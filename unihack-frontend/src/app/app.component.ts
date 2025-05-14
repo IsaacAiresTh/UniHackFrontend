f8a1a81fa0fdf5217fd45547ee1aa0ca0b1ef66f
Isaac-code-maker <playerredbrasil@gmail.com>
2025-05-14 18:47:45 -0300

    adicionando pagina home

--- unihack-frontend/src/app/app.component.ts
+++ unihack-frontend/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  standalone: true,
+  imports: [NavbarComponent, RouterOutlet],
 })
+
 export class AppComponent {
   title = 'unihack-frontend';
 }
--- unihack-frontend/src/app/app.routes.ts
+++ unihack-frontend/src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', component: HomeComponent }, // página inicial
+  { path: 'login', loadComponent: () => import('./auth/login/login.component').then(m => m.LoginComponent) },
 ];

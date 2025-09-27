c48063eb1a95e9be603aaad936779cb7a2f57a6d
IsaacAiresTh <playerredbrasil@gmail.com>
2025-09-27 12:19:18 -0300

    feat: enhance perfil component with user profile display and loading/error states

--- unihack-frontend/src/app/app.routes.ts
+++ unihack-frontend/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { AuthGuard } from './core/auth.guard';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent }, // página inicial
@@ -13,6 +14,14 @@ export const routes: Routes = [
   { path: 'desafios', loadComponent: () => import('./pages/desafios/desafios.component').then(m => m.DesafiosComponent) },
   { path: 'ranking', loadComponent: () => import('./pages/ranking/ranking.component').then(m => m.RankingComponent) }, 
   {path: 'desafios/:id', loadComponent: () => import('./pages/desafio-detalhe/desafio-detalhe.component').then(m => m.DesafioDetalheComponent)},
+  
+  { 
+    path: 'perfil', 
+    loadComponent: () => import('./pages/perfil/perfil.component').then(m => m.PerfilComponent),
+    canActivate: [AuthGuard] // Protege a rota, apenas usuários logados podem acessar
+  },
+  
+  
   // Rota cu ringa para página não encontrada
   { path: '**', redirectTo: '' }
 ];
